b3ca3f05385fa8fb5130a2699d06a4f21d0810d9
jpdiasfernandes <jpdiasfernandes13@gmail.com>
2024-12-04 14:22:40 +0000

    feat: add io plot support for tests graphs

--- action_plugins/hollow_fs.py
+++ action_plugins/hollow_fs.py
@@ -19,36 +19,12 @@ def inc_counter(rep_dict, directory):
     else:
         rep_dict[directory] += 1
 
-def check_report(report_file):
-    report_fd = open(report_file, "r")
-    event_keys = ["thread_id_system", "energy", "event_type"]
-    try:
-        report_json = json.load(report_fd)
-        if isinstance(report_json, list):
-            return False
-        if "events" not in report_json.keys():
-            return False
-        if not isinstance(report_json["events"], list):
-            return False
-        events: list = report_json["events"]
-        if len(events) == 0:
-            return False
-        if not isinstance(events[0], dict):
-            return False
-        event: dict = events[0]
-        for key in event_keys:
-            if key not in event.keys():
-                return False
-        if "thread_info" not in report_json.keys():
-            return False
-
-        tid_info = report_json["thread_info"]
-        if len(tid_info.keys()) == 0:
-            return False
-
-        return True
-    except (json.JSONDecodeError, UnicodeDecodeError):
-        return False
+def match_any_pattern(path_file, patterns):
+    basename: str = os.path.basename(path_file)
+    for pattern in patterns:
+        if re.search(pattern, basename):
+            return True
+    return False
 
 def get_relative_path(absolute_path: str, absolute_base: str):
     if absolute_path.startswith(absolute_base):
@@ -67,21 +43,21 @@ class ActionModule(ActionBase):
             if self._task.check_mode:
                 raise AnsibleActionSkip('check mode not (yet) supported for this module')
 
-
             validation_result, new_module_args = self.validate_argument_spec(
                 argument_spec= {
-                    'base_dir': {'type': 'str'},
-                    'out_base_dir' : {'type': 'str'}
+                    'base_dir': {'type': 'str', 'required': True},
+                    'out_base_dir' : {'type': 'str', 'required' : True},
+                    'patterns' : {'type': 'list', 'elements' : 'str', 'required' : True}
                 },
-                required_together=[
-                    ['base_dir', 'out_base_dir']
-                ]
             )
 
+            stdout_lines = []
             report_mapping = {}
             visited_reports = set()
             base_absolute_path = os.path.abspath(new_module_args['base_dir'])
+            stdout_lines.append(new_module_args['base_dir'])
             out_base_absolute_path = os.path.abspath(new_module_args['out_base_dir'])
+            patterns = new_module_args['patterns']
             #Argument has to be a directory
             if os.path.isfile(out_base_absolute_path):
                 result["error_msg"] = "out_base_dir arg has to be a directory"
@@ -95,27 +71,27 @@ class ActionModule(ActionBase):
 
             for root, dirs, files in os.walk(new_module_args['base_dir']):
                 dir_absolute_path = os.path.abspath(root)
-
                 for file in files:
                     file_absolute_path = dir_absolute_path + '/' + file
+                    stdout_lines.append(file_absolute_path)
+                    stdout_lines.append(base_absolute_path)
                     relative_file_path = get_relative_path(file_absolute_path, base_absolute_path)
                     if relative_file_path:
-                        if check_report(file_absolute_path) and (file_absolute_path not in visited_reports):
+                        stdout_lines.append("Relative path true")
+                        if match_any_pattern(file_absolute_path, patterns) and (file_absolute_path not in visited_reports):
                             report_dir = os.path.dirname(relative_file_path)
-                            report_basename = os.path.basename(relative_file_path)
-                            rep_num = get_rep_num(report_basename)
-                            rep_dir = "graphs_" + str(rep_num)
-                            link_path = out_base_absolute_path + report_dir + "/" +  rep_dir + "/" + report_basename
+                            print(report_dir)
+                            basename = os.path.basename(relative_file_path)
+                            graph_dir = "graphs"
+                            link_path = out_base_absolute_path + report_dir + "/" +  graph_dir + "/" + basename
                             report_mapping[file_absolute_path] = link_path
                             visited_reports.add(file_absolute_path)
-
 
             for original_path, link_path in report_mapping.items():
                 link_dir = os.path.dirname(link_path)
                 os.makedirs(link_dir, exist_ok=True)
                 print(link_path)
                 os.symlink(original_path, link_path)
-
 
         except AnsibleError:
             result['failed'] = True
--- library/plot_energy.py
+++ library/plot_energy.py
@@ -1,4 +1,6 @@
 from __future__ import (absolute_import, division, print_function)
+
+from module_utils.energy_report import DiskPlotter
 __metaclass__ = type
 
 DOCUMENTATION = r'''
@@ -24,6 +26,10 @@ options:
         description: A path to the current working directory
         required: false
         type: str
+    io_file:
+        description: Fine grained disk io activity file path
+        required: false
+        type: str
 
 author:
     - José Pedro Fernandes (@jpdiasfernandes)
@@ -36,6 +42,7 @@ EXAMPLES = r'''
         energy_file: pid-1234.csv
         event_file: rocksdb.log
         chdir: /home/user/tool_results
+        io_file: io.json
 '''
 
 RETURN = r'''
@@ -76,7 +83,8 @@ def run_module():
     module_args = dict(
         report_file=dict(type='str', required=True),
         output_prefix=dict(type='str', required=False),
-        chdir=dict(type='str', required=False)
+        chdir=dict(type='str', required=False),
+        io_file=dict(type='str', required=False)
     )
 
     # seed the result dict in the object
@@ -85,7 +93,7 @@ def run_module():
     # state will include any data that you want your module to pass back
     # for consumption, for example, in a subsequent task
     result = dict(
-        changed=True
+        changed=False
     )
 
     # the AnsibleModule object will be our abstraction working with Ansible
@@ -94,7 +102,7 @@ def run_module():
     # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
-        supports_check_mode=True
+        supports_check_mode=False
     )
 
     # if the user is working with this module in only check mode we do not
@@ -102,7 +110,6 @@ def run_module():
     # state with no modifications
     if module.check_mode:
         module.exit_json(**result)
-
 
     if module.params['chdir'] != None:
         os.chdir(module.params['chdir'])
@@ -113,15 +120,19 @@ def run_module():
     else:
         file_no_format = module.params['output_prefix']
 
-
     report = Report.load_json(module.params['report_file'])
     plotter = ReportPlotter(report)
     plotter.foreground_energy(file_no_format + "-foreground-energy.pdf")
+    result['changed'] = True
     plotter.compaction_energy(file_no_format + "-compaction-energy.pdf")
     plotter.flush_energy(file_no_format + "-flush-energy.pdf")
     plotter.compaction_events_energy(file_no_format + "-compaction-events-energy.pdf", height=20, width=25)
     plotter.compaction_level_report(file_no_format + "-compaction_level_info.pdf")
 
+    if module.params['io_file']:
+        io_file = module.params['io_file']
+        disk_plotter = DiskPlotter(io_file)
+        disk_plotter.plot_compaction_io_info(plotter, 0.1, "events-io-activity.pdf")
     module.exit_json(**result)
 
 def main():
